6564270740fedc11136da4cd9616dcfc4ea2df44
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2020-12-29 06:16:01 +0100

    Modified files

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import Nav from '../nav/nav';
+//import { db} from '../database'
+//import { Cookies } from 'react-cookie'
 
 export default class Settings extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        if(window.matchMedia("(max-width: 767px)").matches){
+            return (
+                <>
+                    <Nav />
+                </>
+            )
+        }else{
+            return (
+                <>
+                    <Nav />
+                </>
+            )
+        }
     }
 }
